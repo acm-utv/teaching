42311090e690a1dcfd5a92c64bcbc47d8f87348f
Giacomo Marciani <giacomo.marciani@gmail.com>
2017-07-25 19:32:02 +0200

    moved foundations of computer science

--- fcs/python/hello.py
+++
@@ -1,9 +0,0 @@
-def standardHello():
-    print "Hello World!"
-
-def customHello(sentence):
-	print sentence
-
-if __name__ == '__main__':
-    standardHello();
-    customHello("Bye bye cruel world!")
